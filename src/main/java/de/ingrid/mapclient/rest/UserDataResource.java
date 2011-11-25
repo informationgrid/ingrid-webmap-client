2bce0210d039bc4e4f5f7959150dad74886cb482
ih <ih@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-11-25 17:28:42 +0000

    renamed file pt2 finally!!

--- src/main/java/de/ingrid/mapclient/rest/UserDataResource.java
+++ src/main/java/de/ingrid/mapclient/rest/UserDataResource.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,6 +15,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
@@ -30,31 +32,44 @@ import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
 import com.thoughtworks.xstream.io.json.JsonWriter;
 
 import de.ingrid.mapclient.UserData;
-import de.ingrid.mapclient.store.FileStore;
+import de.ingrid.mapclient.store.Store;
+import de.ingrid.mapclient.store.StoreManager;
+import de.ingrid.mapclient.store.UserStore;
 
 /**
- * SessionResource defines the interface for retrieving and storing user
- * session data like activate services and the map state
+ * UserDataResource defines the interface for retrieving and storing user
+ * data like activate services and the map state.
  * 
  * @author ingo@wemove.com
  */
-@Path("/session")
-public class SessionResource {
+@Path("/data")
+public class UserDataResource {
 
 	/**
-	 * Get the last saved session data for the current user
-	 * @param req The httpd servlet request
+	 * Path to session data functionality
+	 */
+	private static final String SESSION_PATH = "session";
+
+	/**
+	 * Path to user data functionality
+	 */
+	private static final String USER_PATH = "user";
+
+	/**
+	 * Get the last saved data for the session belonging to the given request
+	 * @param req The httpd servlet request identifying the session
 	 * @return String representing a serialized UserData instance
 	 */
 	@GET
+	@Path(SESSION_PATH)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getLastSessionData(@Context HttpServletRequest req) {
 
-		HttpSession session= req.getSession(true);
+		HttpSession session = req.getSession(true);
 		String sessionId = session.getId();
 
-		FileStore store = FileStore.INSTANCE;
 		try {
+			Store store = StoreManager.INSTANCE.getSessionStore();
 			String xmlData = store.getRecord(sessionId);
 
 			UserData userData = UserData.unserialize(xmlData);
@@ -73,15 +88,19 @@ public class SessionResource {
 	}
 
 	/**
-	 * Store the session data for the current user
+	 * Store the given data for the session belonging to the given request
 	 * @param data representing a serialized UserData instance
 	 * @param req The httpd servlet request
 	 * @throws JSONException
 	 */
 	@POST
+	@Path(SESSION_PATH)
 	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String storeSessionData(String data, @Context HttpServletRequest req) {
+	public void storeSessionData(String data, @Context HttpServletRequest req) {
+
+		HttpSession session= req.getSession(true);
+		String sessionId = session.getId();
+
 		try {
 			// convert json string to UserData
 			JSONObject rootObj = new JSONObject(data);
@@ -99,12 +118,88 @@ public class SessionResource {
 			userData.setActiveServices(activeServices);
 
 			// store the data
-			HttpSession session= req.getSession(true);
-			String sessionId = session.getId();
-
-			FileStore store = FileStore.INSTANCE;
+			Store store = StoreManager.INSTANCE.getSessionStore();
 			store.putRecord(sessionId, userData.serialize());
-			return store.getAbsoluteFilename(sessionId);
+		}
+		catch (Exception ex) {
+			throw new WebApplicationException(ex, Response.Status.SERVICE_UNAVAILABLE);
+		}
+	}
+
+	/**
+	 * List all stored data for the given user id
+	 * @param String containing a JSON encoded array of data
+	 * objects with keys id, title, description
+	 */
+	@GET
+	@Path(USER_PATH+"/{userId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listUserData(@PathParam("userId") String userId) {
+
+		try {
+			UserStore store = StoreManager.INSTANCE.getUserStore();
+			List<String> ids = store.getRecordIds(userId);
+
+			// create the list (we only keep id, title, description and set the other values null)
+			List<UserData> data = new ArrayList<UserData>();
+			for (String id : ids) {
+				String xmlData = store.getRecord(userId, id);
+				UserData userData = UserData.unserialize(xmlData);
+				userData.setId(id);
+				userData.setActiveServices(null);
+				userData.setLocationKey(null);
+				userData.setWmcDocument(null);
+				data.add(userData);
+			}
+			XStream xstream = new XStream(new JsonHierarchicalStreamDriver() {
+				@Override
+				public HierarchicalStreamWriter createWriter(Writer writer) {
+					return new JsonWriter(writer, JsonWriter.DROP_ROOT_MODE);
+				}
+			});
+			String json = xstream.toXML(data);
+			return Response.ok(json).build();
+		}
+		catch (IOException ex) {
+			return null;
+		}
+	}
+
+	/**
+	 * Store the given data for the given user id
+	 * @param data representing a serialized UserData instance
+	 * @param userId The user id
+	 * @throws JSONException
+	 */
+	@POST
+	@Path(USER_PATH+"/{userId}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void storeUserData(String data, @PathParam("userId") String userId) {
+		try {
+			// convert json string to UserData
+			JSONObject rootObj = new JSONObject(data);
+			String title = rootObj.getString("title");
+			String description = rootObj.getString("description");
+			String wmcDocument = rootObj.getString("wmcDocument");
+
+			List<String> activeServices = new ArrayList<String>();
+			JSONArray activeServicesTmp = rootObj.getJSONArray("activeServices");
+			for (int i=0, count=activeServicesTmp.length(); i<count; i++) {
+				String capabilitiesUrl = activeServicesTmp.getString(i);
+				activeServices.add(capabilitiesUrl);
+			}
+
+			UserData userData = new UserData();
+			userData.setTitle(title);
+			userData.setDescription(description);
+			userData.setWmcDocument(wmcDocument);
+			userData.setActiveServices(activeServices);
+
+			// create a unique record id
+			String recordId = UUID.randomUUID().toString();
+
+			UserStore store = StoreManager.INSTANCE.getUserStore();
+			store.putRecord(userId, recordId, userData.serialize());
 		}
 		catch (Exception ex) {
 			throw new WebApplicationException(ex, Response.Status.SERVICE_UNAVAILABLE);
