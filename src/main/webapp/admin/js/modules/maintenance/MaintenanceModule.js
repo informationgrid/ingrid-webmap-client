e97ecc9f769d1daab30b8927049d07abba4ffec2
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-05-22 11:39:57 +0000

    [WEBMAP-CLIENT]: Add new tab.

---
+++ src/main/webapp/admin/js/modules/maintenance/MaintenanceModule.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2011 wemove digital solutions. All rights reserved.
+ */
+Ext.namespace("de.ingrid.mapclient.admin.modules.maintenance");
+
+/**
+ * @class BaseModule provides configuration for the default
+ * map parameters.
+ */
+de.ingrid.mapclient.admin.modules.maintenance.MaintenanceModule = Ext.extend(de.ingrid.mapclient.admin.modules.ModuleBase, {
+
+	title: 'Dienst Wartung',
+	layout: 'accordion',
+	layoutConfig: {
+		titleCollapse: true,
+		animate: false
+	},
+	defaults: {
+		autoScroll: true,
+		padding: 10
+	}
+});
+
+/**
+ * Initialize the component (called by Ext)
+ */
+de.ingrid.mapclient.admin.modules.maintenance.MaintenanceModule.prototype.initComponent = function() {
+	// add the accordion panels
+	Ext.apply(this, {
+		items: [
+		        new de.ingrid.mapclient.admin.modules.maintenance.ServicePanel()
+		        
+		]
+	});
+	de.ingrid.mapclient.admin.modules.maintenance.MaintenanceModule.superclass.initComponent.call(this);
+};
+
+/**
+ * @see de.ingrid.mapclient.admin.modules.ModuleBase.prototype.getId
+ */
+de.ingrid.mapclient.admin.modules.maintenance.MaintenanceModule.prototype.getId = function() {
+	return "maintenance";
+};
+
+/**
+ * Register the module
+ */
+de.ingrid.mapclient.admin.modules.ModuleContainer.registerModule(new de.ingrid.mapclient.admin.modules.maintenance.MaintenanceModule());
---
+++ src/main/webapp/admin/js/modules/maintenance/controls/ServicePanel.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2011 wemove digital solutions. All rights reserved.
+ */
+Ext.namespace("de.ingrid.mapclient.admin.modules.maintenance");
+
+/**
+ * @class ServicePanel is used to manage a list of map projections.
+ */
+de.ingrid.mapclient.admin.modules.maintenance.ServicePanel = Ext.extend(Ext.Panel, {
+
+	title: 'Dienste',
+	layout: 'form',
+	labelAlign: 'top',
+	labelSeparator: '',
+	buttonAlign: 'right',
+	border: false,
+
+	/**
+	 * The ArrayStore for the services
+	 */
+	serviceStore: new Ext.data.ArrayStore({
+		autoDestroy: true,
+		fields: [{
+			name: 'name',
+			type: 'string'
+		}, {
+			name: 'capabilitiesUrl',
+			type: 'string'
+		}, {
+			name: 'category',
+			type: 'string'
+		}]
+	}),
+
+	/**
+	 * The column configuration
+	 */
+	columns: [{
+		header: 'Name',
+		sortable: true,
+		dataIndex: 'name',
+		editor: {
+		   xtype: 'textfield',
+		   allowBlank: false
+		}
+	}, {
+		header: 'URL',
+		sortable: true,
+		dataIndex: 'capabilitiesUrl',
+		editor: {
+		   xtype: 'textfield',
+		   allowBlank: false
+		}
+	}, {
+		header: 'Kategorien',
+		sortable: true,
+		dataIndex: 'category',
+		editor: {
+		   xtype: 'textfield',
+		   allowBlank: false
+		}
+	}]
+});
+
+/**
+ * Initialize the component (called by Ext)
+ */
+de.ingrid.mapclient.admin.modules.maintenance.ServicePanel.prototype.initComponent = function() {
+	
+	de.ingrid.mapclient.admin.modules.maintenance.ServicePanel.superclass.initComponent.call(this);
+};
+
+/**
+ * Render callback (called by Ext)
+ */
+de.ingrid.mapclient.admin.modules.maintenance.ServicePanel.prototype.onRender = function() {
+	de.ingrid.mapclient.admin.modules.maintenance.ServicePanel.superclass.onRender.apply(this, arguments);
+
+	// initialize the projections list
+	var services = de.ingrid.mapclient.Configuration.getValue('serviceCategories');
+	de.ingrid.mapclient.data.StoreHelper.load(this.serviceStore,
+			services, ['name', 'serviceCategories', 'name']);
+};
+
+/**
+ * Save the services list on the server
+ */
+de.ingrid.mapclient.admin.modules.maintenance.ServicePanel.prototype.saveServices = function() {
+	
+};
+
