36cdb7ff6f6b01f33edabfb2df4333c99d47bb80
ma <ma@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-07-18 10:30:41 +0000

    webmapclient: - update of minimap params

--- src/main/webapp/frontend/js/Workspace.js
+++ src/main/webapp/frontend/js/Workspace.js
@@ -624,7 +624,7 @@ de.ingrid.mapclient.frontend.Workspace.prototype.finishInitMap = function() {
         );
 	var ov = new OpenLayers.Control.OverviewMap({
 		layers : [overviewLayer],
-		minRatio: 2, maxRatio: 100
+		minRatio: 30, maxRatio: 30
 		
 	});
 	this.map.addControl(ov);
