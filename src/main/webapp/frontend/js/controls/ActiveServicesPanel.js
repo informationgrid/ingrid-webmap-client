01d1fa6648276974b5b86ae758c23309d575e080
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-06-21 09:29:23 +0000

    [WEBMAP-CLIENT]: config group selection by service selection

--- src/main/webapp/frontend/js/controls/ActiveServicesPanel.js
+++ src/main/webapp/frontend/js/controls/ActiveServicesPanel.js
@@ -403,7 +403,9 @@ de.ingrid.mapclient.frontend.controls.ActiveServicesPanel.prototype.addService =
 		    			});
 		    		}
 		    	} else {
-		    		n.getUI().toggleCheck(checked);
+		    		if(de.ingrid.mapclient.Configuration.getSettings("defaultGroupSelection")){
+		    			n.getUI().toggleCheck(checked);
+		    		}
 		    	}
 		    });
 		});
