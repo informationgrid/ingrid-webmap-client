d22b34cd70703db483c9010ac12d7809f502fe71
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-10-07 10:38:14 +0000

    [WEBMAP-CLIENT]: branches 3.3.x:
    - Set min max x-y-extend if entries has smaller min or max values then capabilities define.

--- src/main/webapp/admin/js/modules/basic/controls/DefaultExtendPanel.js
+++ src/main/webapp/admin/js/modules/basic/controls/DefaultExtendPanel.js
@@ -61,6 +61,89 @@ de.ingrid.mapclient.admin.modules.basic.DefaultExtendPanel.prototype.onRender = 
  * Save the map extend on the server
  */
 de.ingrid.mapclient.admin.modules.basic.DefaultExtendPanel.prototype.saveMapExtend = function() {
-	var mapExtend = this.mapExtendPanel.getExtend();
-	de.ingrid.mapclient.Configuration.setValue('mapExtend', Ext.encode(mapExtend), de.ingrid.mapclient.admin.DefaultSaveHandler);
+	var self = this;
+	var mapExtend = self.mapExtendPanel.getExtend();
+	var wmsCapUrl = de.ingrid.mapclient.Configuration.getValue("wmsCapUrl");
+	
+	Ext.Ajax.request({
+		url: de.ingrid.mapclient.model.WmsProxy.getCapabilitiesUrl(wmsCapUrl),
+		method: 'GET',
+		success: function(response, request) {
+		    var format = new OpenLayers.Format.WMSCapabilities();
+            var capabilities = format.read(response.responseText);
+            if (capabilities.capability) {
+
+                // set up store data
+                var layers = capabilities.capability.layers;
+
+                // get the selected layer names and base layer name from the configuration
+            	var selectedLayers = de.ingrid.mapclient.Configuration.getValue("layers");
+            	var baseLayerName = '';
+            	var layerCheckBBOX; 
+                for (var i=0, count=selectedLayers.length; i<count; i++) {
+                	var layer = selectedLayers[i];
+                	if (layer.isBaseLayer == true) {
+                		baseLayerName = layer.name;
+                	}
+                }
+                
+                if(layers){
+                	for (var i=0, count=layers.length; i<count; i++) {
+                		var layer = layers[i];
+                		if(layer.title == baseLayerName){
+                			layerCheckBBOX = layer;
+                			break;
+                		}
+                	}
+                }
+                
+                if(layerCheckBBOX.llbbox){
+                	var checkBBOX = layerCheckBBOX.llbbox;
+                	if(checkBBOX){
+                		var checkBBOXWest = Math.round(parseFloat(checkBBOX[0]) * 10000) / 10000; // min y
+                		var checkBBOXSouth = Math.round(parseFloat(checkBBOX[1]) * 10000) / 10000; // min x
+                		var checkBBOXEast = Math.floor(parseFloat(checkBBOX[2]) * 10000) / 10000; // max y
+                    	var checkBBOXNorth = Math.floor(parseFloat(checkBBOX[3])* 10000) / 10000; // max x
+
+                    	if(mapExtend){
+                    		// WEST (min y)
+                    		if(mapExtend.west < checkBBOXWest){
+                    			mapExtend.west = checkBBOXWest;
+                    		}else if(mapExtend.west > checkBBOXEast){
+	                	  		mapExtend.west = checkBBOXWest;
+	                	  	}
+                    		
+                    		// SOUTH (min x)
+	                    	if(mapExtend.south < checkBBOXSouth){
+	                    		mapExtend.south = checkBBOXSouth;
+	                	  	}else if(mapExtend.south > checkBBOXNorth){
+	                	  		mapExtend.south = checkBBOXSouth;
+	                	  	}
+	                    	
+	                    	// EAST (max y)
+	                    	if(mapExtend.east > checkBBOXEast){
+	                    		mapExtend.east = checkBBOXEast;
+	                	  	}else if(mapExtend.east < checkBBOXWest){
+	                	  		mapExtend.east = checkBBOXEast;
+	                	  	}
+	                    	
+	                    	// NORTH (max x)
+	                    	if(mapExtend.north > checkBBOXNorth){
+	                    		mapExtend.north = checkBBOXNorth;
+	                	  	}else if(mapExtend.north < checkBBOXSouth){
+	                	  		mapExtend.north = checkBBOXNorth;
+	                	  	}
+                    	}
+                	}
+                }
+                de.ingrid.mapclient.Configuration.setValue('mapExtend', Ext.encode(mapExtend), de.ingrid.mapclient.admin.DefaultSaveHandler);
+                self.mapExtendPanel.setExtend(mapExtend.north, mapExtend.west, mapExtend.east, mapExtend.south);
+            } else {
+            	de.ingrid.mapclient.Message.showError(de.ingrid.mapclient.Message.LOAD_CAPABILITIES_FAILURE);
+            }
+		},
+		failure: function(response, request) {
+			de.ingrid.mapclient.Message.showError(de.ingrid.mapclient.Message.LOAD_CAPABILITIES_FAILURE);
+		}
+	});
 };
