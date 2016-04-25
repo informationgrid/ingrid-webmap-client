3243584f396fa0123f4748b1669c0ae5fe3b7291
Toan Tran <khanh.toan.tran@wemove.com>
2016-04-25 10:09:23 +0200

    Update compile script.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 if [ -d "mf-geoadmin3" ] ; then
-    echo "RESET REPO"
+    echo "*** RESET REPO ***"
     cd ./mf-geoadmin3
     
 	# reset mf-geoadmin3 changes
@@ -9,16 +9,18 @@ if [ -d "mf-geoadmin3" ] ; then
 	# clean mf-geoadmin3 build path
 	make cleanall 
 else
-	echo "CLONE REPO"
+	echo "*** CLONE REPO ***"
     # clone repo
 	git clone https://github.com/geoadmin/mf-geoadmin3.git mf-geoadmin3
 	cd ./mf-geoadmin3
 fi
 
 # checkout mf-geoadmin3 state
+echo "*** Checkout mf-geoadmin3 revision ***"
 git checkout 3a43911a12584353a6ea2422e5282ade258369b3
 	
 # copy ingrid files to mf-geoadmin3/src
+echo "*** Copy files ***"
 cp -r ../src/main/webapp/frontend/ingrid/scripts ./
 cp -r ../src/main/webapp/frontend/ingrid/lib src/
 cp -r ../src/main/webapp/frontend/ingrid/components src/
@@ -27,19 +29,22 @@ cp -r ../src/main/webapp/frontend/ingrid/style src/
 cp -r ../src/main/webapp/frontend/ingrid/img src/
 cp -r ../src/main/webapp/frontend/ingrid/locales src/
 cp -r ../src/main/webapp/frontend/src/index.mako.html src/
-cp -r ../src/main/webapp/frontend/src/rc_ingrid ./
 
 # Add environments
+echo "*** Export environments ***"
 export DEFAULT_TOPIC_ID=themen
 export DEFAULT_EXTENT="'[0.42587260523, 46.9672880527, 15.7908768234, 55.1764096793]'"
 export DEFAULT_EPSG="EPSG:3857"
 export DEFAULT_EPSG_EXTEND="'[0.42587260523, 46.9672880527, 15.7908768234, 55.1764096793]'"
 
 # make libs
+echo "*** Make libs ***"
 make libs
 
 # make dev version
+echo "*** Make debug ***"
 make debug
 
 # make prod version
+echo "*** Make release ***"
 make release
