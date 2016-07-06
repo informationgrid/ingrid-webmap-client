a612f80ee1141af6f06872b38d5cdeb9ffb957dc
Toan Tran <khanh.toan.tran@wemove.com>
2016-07-06 17:07:32 +0200

    Update mouse position.

--- src/main/webapp/frontend/ingrid/js/MousePositionController.js
+++ src/main/webapp/frontend/ingrid/js/MousePositionController.js
@@ -23,57 +23,49 @@ goog.provide('ga_mouseposition_controller');
           value: 'EPSG:3857',
           label: 'Mercator (Breite/Länge)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2);
           }
         }, {
           value: 'EPSG:4326',
           label: 'WGS 84 (Breite/Länge)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 5) + ' (WGS 84)';
           }
         }, {
           value: 'EPSG:31466',
           label: 'GK2 - DHDN90 (Rechtswert/Hochwert)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (GK2)';
           }
         }, {
           value: 'EPSG:31467',
           label: 'GK3 - DHDN90 (Rechtswert/Hochwert)',
           format: function(coordinates) {
-             return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+             return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (GK3)';
           }
         }, {
           value: 'EPSG:31468',
           label: 'GK4 - DHDN90 (Rechtswert/Hochwert)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (GK4)';
           }
         }, {
           value: 'EPSG:31469',
           label: 'GK5 - DHDN90 (Rechtswert/Hochwert)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (GK5)';
           }
         }, {
           value: 'EPSG:25832',
           label: 'UTM 32N - ETRS89 (East/North)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (UTM 32N)';
           }
         }, {
           value: 'EPSG:25833',
           label: 'UTM 33N - ETRS89 (East/North)',
           format: function(coordinates) {
-            return ol.coordinate.toStringHDMS(coordinates) +
-                ' (' + ol.coordinate.format(coordinates, '{y}, {x}', 5) + ')';
+            return ol.coordinate.format(coordinates, '{y}, {x}', 2) + ' (UTM 33N)';
           }
         }
         ];
