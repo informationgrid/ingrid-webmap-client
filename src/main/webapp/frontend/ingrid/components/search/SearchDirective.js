acc302d38ea4504f88ea83976cb06e3095bf1010
Toan Tran <ktt@wemove.com>
2017-09-21 15:33:19 +0200

    Fix lint.

--- src/main/webapp/frontend/ingrid/components/search/SearchDirective.js
+++ src/main/webapp/frontend/ingrid/components/search/SearchDirective.js
@@ -179,9 +179,10 @@ goog.require('ga_what3words_service');
             w3w.then(function(response) {
               var res = response.data;
               if (res && res.geometry && res.geometry.lng && res.geometry.lat) {
+                // INGRID: Change projection
                 var newPos = ol.proj.transform([res.geometry.lng,
-                                                res.geometry.lat],
-                                               'EPSG:4326', $scope.map.getView().getProjection().getCode());
+                  res.geometry.lat],
+                  'EPSG:4326', $scope.map.getView().getProjection().getCode());
                 gaMapUtils.moveTo($scope.map, $scope.ol3d, 8, newPos);
                 gaMarkerOverlay.add($scope.map, newPos, true);
               }
