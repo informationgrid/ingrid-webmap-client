5d57b4bd70fa33411c177422f42e77f02edf6f70
Toan Tran <khanh.toan.tran@wemove.com>
2018-03-05 12:13:14 +0100

    ITZ-Bund: Update setting graticule layer to disable option.

--- src/main/webapp/frontend/profile_settings/baw_wsv/config/setting.profile.js
+++ src/main/webapp/frontend/profile_settings/baw_wsv/config/setting.profile.js
@@ -34,11 +34,14 @@ settingShowISOXML = false;
 settingSearchCoordsXY = true;
 settingPrintFilename = 'Print.GeoPortal.WSV';
 settingPrintDependOnMouseProj = true;
+settingPrintGraticuleLayer = '{}';
+/*
 settingPrintGraticuleLayer = '{' + 
   '"EPSG:4326":{"url":"http://atlas.wsv.bund.de/netze/wms?", "layers":["GN","GNB"]},'+
   '"EPSG:25832":{"url":"http://atlas.wsv.bund.de/netze/wms?", "layers":["GN","GNB"]},'+
   '"EPSG:25833":{"url":"http://atlas.wsv.bund.de/netze/wms?", "layers":["GN","GNB"]}'+
 '}';
+*/
 settingShareFacebook = false;
 settingShareGoogle = false;
 settingShareTwitter = false;
