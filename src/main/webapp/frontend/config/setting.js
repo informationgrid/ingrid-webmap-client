7bb9178a1b1d47c930d0b3feaf23956e9e3fdbd8
Toan Tran <khanh.toan.tran@wemove.com>
2018-08-20 14:20:56 +0200

    Format code syntax

--- src/main/webapp/frontend/config/setting.js
+++ src/main/webapp/frontend/config/setting.js
@@ -34,10 +34,10 @@ var settingSearchBwaLocatorGeoUrl = 'https://atlas.wsv.bund.de/bwastr-locator/re
 // Define BWaStrLocator services URL for river details
 var settingSearchBwaLocatorStationUrl = 'https://atlas.wsv.bund.de/bwastr-locator/rest/stationierung/query';
 
-//Override copyright URL (Default: '/datenschutzbestimmung')
+// Override copyright URL (Default: '/datenschutzbestimmung')
 var settingCopyrightURL = undefined;
 
-//Override disclaimer URL (Default: '/impressum')
+// Override disclaimer URL (Default: '/impressum')
 var settingDisclaimerURL = undefined;
 
 // Override sitemap URL (Default: '/inhaltsverzeichnis')
