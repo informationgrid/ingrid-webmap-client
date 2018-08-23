f034d0bc962ae187d164bbab3c6764b8b9f153db
Toan Tran <khanh.toan.tran@wemove.com>
2018-08-23 10:29:22 +0200

    Change HttpProxy request function

--- src/main/java/de/ingrid/mapclient/rest/WmsResource.java
+++ src/main/java/de/ingrid/mapclient/rest/WmsResource.java
@@ -103,29 +103,31 @@ public class WmsResource {
     public String doWmsRequest(@QueryParam("url") String url, @QueryParam("toJson") boolean toJson) {
         try {
             String response = HttpProxy.doRequest( url );
-            if (url.toLowerCase().indexOf( "getfeatureinfo" ) > 0) {
-                // Remove script tags on getFeatureInfo response.
-                Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>",
-                        Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-                return p.matcher(response).replaceAll("");
-            } else {
-                // Replace "," to "." on bounding box.
-                response = response.replaceAll( "x=\"([0-9]+),([0-9]+)\"", "x=\"$1.$2\"" );
-                response = response.replaceAll( "y=\"([0-9]+),([0-9]+)\"", "y=\"$1.$2\"" );
-                response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
-                response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
-            }
-            if(toJson){
-                JSONObject json;
-                try {
-                    json = XML.toJSONObject( response );
-                } catch (JSONException e) {
-                    json = new JSONObject();
+            if(response != null) {
+                if (url.toLowerCase().indexOf( "getfeatureinfo" ) > 0) {
+                    // Remove script tags on getFeatureInfo response.
+                    Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>",
+                            Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+                    return p.matcher(response).replaceAll("");
+                } else {
+                    // Replace "," to "." on bounding box.
+                    response = response.replaceAll( "x=\"([0-9]+),([0-9]+)\"", "x=\"$1.$2\"" );
+                    response = response.replaceAll( "y=\"([0-9]+),([0-9]+)\"", "y=\"$1.$2\"" );
+                    response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
+                    response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
                 }
-                json.put( "xmlResponse", response );
-                return json.toString();
+                if(toJson){
+                    JSONObject json;
+                    try {
+                        json = XML.toJSONObject( response );
+                    } catch (JSONException e) {
+                        json = new JSONObject();
+                    }
+                    json.put( "xmlResponse", response );
+                    return json.toString();
+                }
+                return response;
             }
-            return response;
         } catch (IOException ex) {
             log.error( "Error sending WMS request: " + url, ex );
             throw new WebApplicationException( ex, Response.Status.NOT_FOUND );
