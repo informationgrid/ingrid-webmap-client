49e897aa2113b8f96a5d04410b02a884ce3da6c2
Toan Tran <khanh.toan.tran@wemove.com>
2018-08-28 17:09:07 +0200

    Check wms response

--- src/main/java/de/ingrid/mapclient/rest/WmsResource.java
+++ src/main/java/de/ingrid/mapclient/rest/WmsResource.java
@@ -122,29 +122,31 @@ public class WmsResource {
             } else {
                 response = HttpProxy.doRequest( url );
             }
-            if(response.indexOf("<?xml") == -1) {
-               response = "<?xml version=\"1.0\" encoding=\"UTF-8\">" + response;
-            }
-            if (url.toLowerCase().indexOf( "getfeatureinfo" ) > 0) {
-                // Remove script tags on getFeatureInfo response.
-                Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>",
-                        Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-                return p.matcher(response).replaceAll("");
-            } else {
-                // Replace "," to "." on bounding box.
-                response = response.replaceAll( "x=\"([0-9]+),([0-9]+)\"", "x=\"$1.$2\"" );
-                response = response.replaceAll( "y=\"([0-9]+),([0-9]+)\"", "y=\"$1.$2\"" );
-                response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
-                response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
-            }
-            if(toJson){
-                JSONObject json;
-                try {
-                    json = XML.toJSONObject( response );
-                    json.put( "xmlResponse", response );
-                    return json.toString();
-                } catch (JSONException e) {
-                    log.error("Error create json object" + response);
+            if(response != null) {
+                if(response.indexOf("<?xml") == -1) {
+                   response = "<?xml version=\"1.0\" encoding=\"UTF-8\">" + response;
+                }
+                if (url.toLowerCase().indexOf( "getfeatureinfo" ) > 0) {
+                    // Remove script tags on getFeatureInfo response.
+                    Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>",
+                            Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+                    return p.matcher(response).replaceAll("");
+                } else {
+                    // Replace "," to "." on bounding box.
+                    response = response.replaceAll( "x=\"([0-9]+),([0-9]+)\"", "x=\"$1.$2\"" );
+                    response = response.replaceAll( "y=\"([0-9]+),([0-9]+)\"", "y=\"$1.$2\"" );
+                    response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
+                    response = response.replaceAll( "tude>([0-9]+),([0-9]+)", "tude>$1.$2" );
+                }
+                if(toJson){
+                    JSONObject json;
+                    try {
+                        json = XML.toJSONObject( response );
+                        json.put( "xmlResponse", response );
+                        return json.toString();
+                    } catch (JSONException e) {
+                        log.error("Error create json object" + response);
+                    }
                 }
             }
             return response;
