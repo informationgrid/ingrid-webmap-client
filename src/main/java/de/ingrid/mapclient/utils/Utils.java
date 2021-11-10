173bdf2390a7a73491764396a2fc19d7f2546862
Toan Tran <khanh.toan.tran@wemove.com>
2021-11-10 13:40:46 +0100

    Cleanup DocumentBuilderFactory features
    (cherry picked from commit b49bda54a232e9eee2cd085b2776881cc988e8f6)
    (cherry picked from commit fe2d87585cf7a00143b4e84574e6624d75d6f138)
    (cherry picked from commit 19916018aed5b3d7fe81470e6a269a044f2e6b7f)
    (cherry picked from commit 282a98c9830d0b18aea5af601c71dc09d854b49a)

--- src/main/java/de/ingrid/mapclient/utils/Utils.java
+++ src/main/java/de/ingrid/mapclient/utils/Utils.java
@@ -314,9 +314,7 @@ public class Utils {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
         factory.setValidating(false);
-        factory.setNamespaceAware(true);
-        factory.setFeature("http://xml.org/sax/features/namespaces", false);
-        factory.setFeature("http://xml.org/sax/features/validation", false);
+        factory.setNamespaceAware(false);
         factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
         factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
         factory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
