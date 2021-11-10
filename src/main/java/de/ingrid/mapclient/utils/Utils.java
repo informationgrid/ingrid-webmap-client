5f24c49bc4a55db010ec90e9146dc7688adc12be
Toan Tran <khanh.toan.tran@wemove.com>
2021-11-10 09:19:31 +0100

    Fix cherrypick
    (cherry picked from commit 16ab0df5b0ff0c68167925d4a11aff6966384580)

--- src/main/java/de/ingrid/mapclient/utils/Utils.java
+++ src/main/java/de/ingrid/mapclient/utils/Utils.java
@@ -31,6 +31,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
