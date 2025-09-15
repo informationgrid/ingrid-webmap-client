c6aff00fe1b812260bb8808fb139c656a64f71e6
Toan Tran <khanh.toan.tran@wemove.com>
2025-09-15 11:11:26 +0200

    Add env SERVER_CONNECTOR_ATTR
    (cherry picked from commit 625b78e40c97a6ae710cf1b9f336d89617a57480)
    (cherry picked from commit dd4fa4a86c4dc7050b2a45eebe5f37edd16b4069)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ "$SERVER_CONNECTOR_ATTR" ]; then
+    echo "Update connector attributes"
+    sed -i -e "s@redirectPort=\"8443\" />@redirectPort=\"8443\" ${SERVER_CONNECTOR_ATTR} />@" conf/server.xml
+fi
+
 exec "$@"
