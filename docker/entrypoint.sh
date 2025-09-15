f29bd9a41cdfe20c092522670cfc426b74ebe62a
Toan Tran <khanh.toan.tran@wemove.com>
2025-09-15 16:34:32 +0200

    Update entrypoint sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$SERVER_CONNECTOR_ATTR" ]; then
     echo "Update connector attributes"
-    sed -i -e "s@redirectPort=\"8443\"\r@redirectPort=\"8443\" ${SERVER_CONNECTOR_ATTR}@" conf/server.xml
+    sed -i -e "s@<Connector port=\"8080\" protocol=\"HTTP/1.1\"@<Connector port=\"8080\" protocol=\"HTTP/1.1\" ${SERVER_CONNECTOR_ATTR}@" conf/server.xml
 fi
 
 exec "$@"
