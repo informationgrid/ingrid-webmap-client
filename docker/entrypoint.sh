e84b3a242bef1e62efb2f3659c204d459d0f2812
Toan Tran <khanh.toan.tran@wemove.com>
2025-11-06 15:09:36 +0100

    Add env MAPCLIENT_ADMIN_PW
    (cherry picked from commit c88d008c305a8ec6509beaaac27af29696ad0b46)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,10 @@ if [ "$SERVER_CONNECTOR_ATTR" ]; then
     sed -i -e "s@<Connector port=\"8080\" protocol=\"HTTP/1.1\"@<Connector port=\"8080\" ${SERVER_CONNECTOR_ATTR} protocol=\"HTTP/1.1\"@" conf/server.xml
 fi
 
+# Change admin password for mapclient admin GUI if MAPCLIENT_ADMIN_PW is define
+if [ "$MAPCLIENT_ADMIN_PW" ]; then
+    echo "Update mapclient admin password"
+    sed -i 's|password="8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918" roles="admin-gui|password="'${MAPCLIENT_ADMIN_PW}'" roles="admin-gui|' conf/tomcat-users.xml
+fi
+
 exec "$@"
